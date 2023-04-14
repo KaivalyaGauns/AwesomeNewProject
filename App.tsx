be3a49cee84124c3270529137ef4eacdc2ab47c8
Kaivalya Gauns <kaivalyagauns@ip-192-168-3-42.ap-south-1.compute.internal>
2023-04-14 18:22:07 +0530

    implemented webview and app communication

--- App.tsx
+++ App.tsx
@@ -1,117 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { Button, Linking, StyleSheet, Modal, View, Text } from 'react-native';
+import { InAppBrowser } from 'react-native-inappbrowser-reborn';
+import WebView from 'react-native-webview';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const WEB_PAGE_URL = 'http://localhost:8080/';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const [text, setText] = useState("NO TEXTT");
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const openLinkInBrowserHandler = () => {
+    Linking.canOpenURL(WEB_PAGE_URL).then((supported) => {
+      supported && Linking.openURL(WEB_PAGE_URL);
+    });
+  };
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const openLinkInWebView = () => setVisible(true);
+
+  const openLinkInAppBrowser = () => {
+    InAppBrowser.isAvailable()
+      .then(() => {
+        return InAppBrowser.open(WEB_PAGE_URL, {
+          // iOS Properties
+          animated: true,
+          modalEnabled: true,
+          // Android Properties
+          showTitle: true,
+        });
+      })
+      .catch((_) => openLinkInBrowserHandler());
+  };
+
+  const INJECTED_JAVASCRIPT = `(function() {
+    setInterval(()=> {
+      window.postMessage(JSON.stringify(window.location));
+    },1000)
+  })();`;
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+    <View style={styles.container}>
+      <Button
+        title={'Open link in Browser'}
+        onPress={openLinkInBrowserHandler}
+      />
+      <View style={styles.separator} />
+      <Button title={'Open link in WebView'} onPress={openLinkInWebView} />
+      <View style={styles.separator} />
+      <Button
+        title={'Open link in InAppBrowser'}
+        onPress={openLinkInAppBrowser}
+      />
+      <Modal
+        visible={visible}
+        presentationStyle={'pageSheet'}
+        animationType={'slide'}
+        onRequestClose={() => setVisible(!visible)}
+      >
+        <WebView 
+          source={{ uri: WEB_PAGE_URL }}
+          injectedJavaScript={INJECTED_JAVASCRIPT}
+          onMessage={(event)=>{
+            let message  = event.nativeEvent.data;
+            /* event.nativeEvent.data must be string, i.e. window.postMessage
+            should send only string.
+            * */
+            setText(message);
+          }}
+          domStorageEnabled
+          javaScriptEnabled
+          originWhitelist={['*']}
+        />
+      </Modal>
+      <Text>
+        {text}
       </Text>
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  separator: {
+    height: 20,
   },
 });
 
